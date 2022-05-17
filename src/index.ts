196c3b9ee0d16216439556901a6007be470cbb31
Xie Yuheng <xyheme@gmail.com>
2022-05-17 11:38:28 +0800

    up

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from "./command"
 export * from "./command-runner"
-export * as CommandRunners from "./command-runner"
+export * as CommandRunners from "./command-runners"
 export * as Commands from "./commands"
